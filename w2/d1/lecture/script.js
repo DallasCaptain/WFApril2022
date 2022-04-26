67e79a2747c62efb5e815617a1638e4965218c3f
Chris Thompson <cthompson@codingdojo.com>
2022-04-26 09:02:52 -0500

    w2d1 afternoon lecture - js OBJECTS

--- w2/d1/lecture/script.js
+++ w2/d1/lecture/script.js
@@ -59,16 +59,42 @@ function clicked(){
     cla.innerText = 'SDFSDFSDFSDF'
 
     h1.style.backgroundColor = 'firebrick';
-    h1.onmouseover=function(){
-        h1.style.backgroundColor='green'
-    }
-    h1.onmouseout=function(){
-        h1.style.backgroundColor='black'
+    // h1.onmouseover=function(){
+    //     h1.style.backgroundColor='green'
+    // }
+    // h1.onmouseout=function(){
+    //     h1.style.backgroundColor='black'
+    // }
+}
+
+function addclass(){
+    //show you the classes on the h1
+    var h1 = document.querySelector('h1')
+    console.log(h1.classList)
+    if(h1.classList.contains('red')){
+        console.log('it already has red')
+    }else{
+        h1.classList.add('red')
+        h1.classList.remove('bob')
     }
 }
 
+function firstclass(){
+    var h1 = document.querySelector('h1')
+    console.log(h1.classList[0])
+
+}
+
+function showname(input){
+    console.log(input.value)
+}
+
+function myalert(){
+    alert('This waited 3 seconds')
+}
 
 
+setTimeout(myalert,3000)
 
 
 
